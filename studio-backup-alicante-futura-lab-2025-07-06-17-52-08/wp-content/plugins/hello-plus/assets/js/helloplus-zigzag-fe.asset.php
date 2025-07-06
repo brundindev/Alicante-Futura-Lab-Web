b449b6fb6af7e742899965949636ebc213af5c9b
brundindev <134782358+brundindev@users.noreply.github.com>
2025-07-06 17:54:54 +0200

    Start

---
+++ studio-backup-alicante-futura-lab-2025-07-06-17-52-08/wp-content/plugins/hello-plus/assets/js/helloplus-zigzag-fe.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'dbffe97df6546f1ff914');
---
+++ studio-backup-alicante-futura-lab-2025-07-06-17-52-08/wp-content/plugins/hello-plus/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/hello-plus',
+        'pretty_version' => '1.6.2.x-dev',
+        'version' => '1.6.2.9999999-dev',
+        'reference' => '2d75667b5d0ce74912c41bb937850d4918a6f51e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/hello-plus' => array(
+            'pretty_version' => '1.6.2.x-dev',
+            'version' => '1.6.2.9999999-dev',
+            'reference' => '2d75667b5d0ce74912c41bb937850d4918a6f51e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'elementor/wp-notifications-package' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => 'dd25ca9dd79402c3bb51fab112aa079702eb165e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elementor/wp-notifications-package',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
