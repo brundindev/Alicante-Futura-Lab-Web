bf3910411376806f769980a11699e8f89de296dc
brundindev <134782358+brundindev@users.noreply.github.com>
2025-07-07 23:39:30 +0200

    Fix responsive

--- studio-backup-alicante-futura-lab-2025-07-07-16-24-02/wp-content/plugins/hello-plus/assets/js/helloplus-forms-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '0458f8757ee8dc44628c');
---
+++ studio-backup-alicante-futura-lab-2025-07-07-23-39-00/wp-content/plugins/hello-plus/assets/css/helloplus-header.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b13ee1653573d62c3c22');
